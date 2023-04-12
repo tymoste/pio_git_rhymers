0b544c934b629b0f33a88b385d3956109513d953
tymoste <tymoste@gmail.com>
2023-04-12 12:32:35 +0200

    4.2 Poprawa konwencji nazewniczej metod

--- src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/Rhymersfactory.java
@@ -1,7 +1,8 @@
 package edu.kis.vh.nursery.factory;
 
-import edu.kis.vh.nursery.defaultCountingOutRhymer;
+import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 
+<<<<<<< HEAD
 public interface Rhymersfactory {
 
     public defaultCountingOutRhymer GetStandardRhymer();
@@ -13,3 +14,16 @@ public interface Rhymersfactory {
     public defaultCountingOutRhymer GetHanoiRhymer();
 
 }
+=======
+    public interface Rhymersfactory {
+    
+        public DefaultCountingOutRhymer getStandardRhymer();
+        
+        public DefaultCountingOutRhymer getFalseRhymer();
+        
+        public DefaultCountingOutRhymer getFIFORhymer();
+        
+        public DefaultCountingOutRhymer getHanoiRhymer();
+        
+    }
+>>>>>>> 0ceefe8 (4.2 Poprawa konwencji nazewniczej metod)
