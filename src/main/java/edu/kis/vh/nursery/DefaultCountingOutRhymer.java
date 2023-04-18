7a05ee7b75e6d280739b0fde59f08e05408c2b09
Artur Trębski <240822@edu.p.lodz.pl>
2023-04-19 01:01:00 +0200

    6. Zastąpienie deklaracjami stałych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,10 +1,14 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
+    public static final int MAX_SIZE = 12;
+    public static final int EMPTY = -1;
+    public static final int FULL = 11;
+    final int RETURN_VALUE = -1;
 
-    private int[] numbers = new int[12];
+    private int[] numbers = new int[MAX_SIZE];
 
-    public int total = -1;
+    public int total = EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +16,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return RETURN_VALUE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return RETURN_VALUE;
         return numbers[total--];
     }
 
