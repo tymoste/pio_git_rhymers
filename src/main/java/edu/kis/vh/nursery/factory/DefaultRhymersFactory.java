e012d78320126d3c83bfab71656be1e7a50b056b
= <=>
2023-05-10 13:24:01 +0200

    Wygenerowano szkielet dokumentacji dla klasy DefaultRhymersFactory

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -6,21 +6,37 @@ import edu.kis.vh.nursery.HanoiRhymer;
 
 public class DefaultRhymersFactory implements Rhymersfactory {
 
+    /**
+     *
+     * @return Returns new DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getStandardRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     *
+     * @return Returns new DefaultCountingOutRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFalseRhymer() {
         return new DefaultCountingOutRhymer();
     }
 
+    /**
+     *
+     * @return Returns new FIFORhymer
+     */
     @Override
     public DefaultCountingOutRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
+    /**
+     *
+     * @return Returns new HanoiRhymer
+     */
     @Override
     public DefaultCountingOutRhymer getHanoiRhymer() {
         return new HanoiRhymer();
